458f9c01beb13c96ccb31d65c93218a8a18ac5cf
belfaqir <belfaqir_imaf@yahoo.fr>
2021-01-31 05:09:59 +0100

    commit

--- cucumber_tuto_replay/src/test/java/cucumber_tuto_replay/StepDefinitionNewProject.java
+++ cucumber_tuto_replay/src/test/java/cucumber_tuto_replay/StepDefinitionNewProject.java
@@ -4,6 +4,8 @@ public class StepDefinitionNewProject {
 	public void display() {
 	System.out.println("here we are");
 	System.out.println("here we are vvvv");
+	System.out.println("here we are vvv cvc v");
+	System.out.println("here we are vvv cv    cvvcvcv c v");
 
 //	System.out.println("i am in welcome page du site de recherche google");
 	}
