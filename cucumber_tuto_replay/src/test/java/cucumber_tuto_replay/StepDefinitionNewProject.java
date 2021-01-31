0c819ae495517c00edaf7d24e16c043eedd3269c
belfaqir <belfaqir_imaf@yahoo.fr>
2021-01-31 04:38:51 +0100

    add display function

--- cucumber_tuto_replay/src/test/java/cucumber_tuto_replay/StepDefinitionNewProject.java
+++ cucumber_tuto_replay/src/test/java/cucumber_tuto_replay/StepDefinitionNewProject.java
@@ -1,6 +1,9 @@
 package cucumber_tuto_replay;
 
 public class StepDefinitionNewProject {
-	
+	public void display() {
+	System.out.println("here we are");
+//	System.out.println("i am in welcome page du site de recherche google");
+	}
 
 }
